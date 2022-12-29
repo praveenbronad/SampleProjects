f028dd969786638c98490c6da3cd8f0045d78726
Praveen B Ronad <Praveen B Ronad@LAPTOP-LCLJAJ2G>
2022-12-29 12:44:28 +0530

    hi

---
+++ src/test/java/scripts/TestCase1.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+@Test
+public void test()
+{
+	Reporter.log("python",true);
+}
+}
---
+++ src/test/java/scripts/TestCase2.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+@Test
+public void test()
+{
+	Reporter.log("java",true);
+}
+}
